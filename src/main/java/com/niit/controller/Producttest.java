f079b3297ab8f97d9de537c970c9e4ec606e64be
MEHRAJ <MEHRAJ@DESKTOP-UBB91SL>
2016-08-09 13:26:48 +0530

    first commit

---
+++ src/main/java/com/niit/controller/Producttest.java
@@ -0,0 +1,15 @@
+package com.niit.controller;
+
+public class Producttest {
+   public static void main(String[] args) {
+	product p = new product();
+	p.setId(67);
+	p.setName("htc");
+	p.setPrice(35000);
+	System.out.println(p.getId());
+	System.out.println(p.getName());
+	System.out.println(p.getPrice());
+	
+}
+
+}
